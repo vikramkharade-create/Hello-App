64b9d74a491e231fe53df7bc551faa50c038d8af
sd4850 <sd4850@srmist.edu.in>
2026-04-10 13:22:01 +0530

    Create HelloApp2.java

---
+++ HelloApp2.java
@@ -0,0 +1,7 @@
+public class HelloApp2 {
+    public static void main(String[] args) {
+        // Get the name from command-line argument
+        String name = args[0];
+        System.out.println("Hello, " + name + "!");
+    }
+}
